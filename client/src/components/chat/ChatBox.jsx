dee21ef8893397cdcfb35cb4ac0b90cb0e2163dd
fardeen077 <fardeen36143@gmail.com>
2026-03-07 22:28:02 +0530

    feat: add ChatBox and Messages components; integrate UsersList in Home page layout

---
+++ client/src/components/chat/ChatBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Messages from "./Messages"
+function ChatBox() {
+    return (
+        <div className='bg-blue-600 w-full'>
+            <Messages/>
+        </div>
+    )
+}
+
+export default ChatBox
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import UsersList from '../components/sidebar/UsersList'
+import ChatBox from '../components/chat/ChatBox'
 function Home() {
     return (
-        <div>
-            <div>
-                <Navbar/>
+            <div className="flex items-center justify-center pt-20 px-4">
+                <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+                    <div className="flex h-full rounded-lg overflow-hidden">
+                        <UsersList />
+                        <ChatBox />
+                    </div>
+                </div>
             </div>
-            Chat App
-
-        </div>
     )
 };
 
